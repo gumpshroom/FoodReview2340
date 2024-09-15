6c376446173c4eb59d65a74f707d77ccbb1d9605
Nathan Nguyen <nnguyen11262004@gmail.com>
2024-09-15 17:41:40 -0400

    Map with multiple locations established.

--- foodFind/views.py
+++ foodFind/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'FoodReview2340/home.html')
+def map(request):
+    return render(request, 'FoodReview2340/mapTest.html')
