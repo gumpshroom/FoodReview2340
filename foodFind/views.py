104390fb781b248bd7b5f77ebedeb5de686c0f78
Eric Yang <eyang2023@gmail.com>
2024-09-15 18:18:25 -0400

    add homepage styling

--- foodFind/views.py
+++ foodFind/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'FoodReview2340/home.html')
+    return render(request, 'FoodReview2340/homepage.html')
