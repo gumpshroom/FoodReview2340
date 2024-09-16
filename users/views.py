591aa7e7f2435142aaef253c18b3b37367b0ca36
Nathan Nguyen <nnguyen11262004@gmail.com>
2024-09-15 23:41:44 -0400

    Reapply "Merge branch 'Nathan's-Branch'"
    This reverts commit 64fb127591fc332d42d71bc9c2bdecae49068b6c.

--- users/views.py
+++ users/views.py
@@ -1,31 +1,16 @@
-from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from .forms import UserRegisterForm
 from django.contrib import messages
 
-def register_view(request):
+def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            login(request, form.save())
+            user = form.save()  # Save the user
+            login(request, user)  # Automatically log the user in after registration
             messages.success(request, f'Your account has been created!')
-            return redirect('/foodFind/')
+            return redirect('home')  # Redirect to home or another page
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            login(request, form.get_user())
-            return redirect('/foodFind/')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'users/login.html', {'form': form})
-
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('/foodFind/')
