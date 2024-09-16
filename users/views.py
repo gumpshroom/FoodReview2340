473daeb664937076723c58571cc648882543222b
Nathan Nguyen <nnguyen11262004@gmail.com>
2024-09-16 00:08:45 -0400

    fixed the messups

--- users/views.py
+++ users/views.py
@@ -1,16 +1,31 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import UserRegisterForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
-def register(request):
+def register_view(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()  # Save the user
-            login(request, user)  # Automatically log the user in after registration
+            login(request, form.save())
             messages.success(request, f'Your account has been created!')
-            return redirect('home')  # Redirect to home or another page
+            return redirect('/foodFind/')
     else:
-        form = UserRegisterForm()
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/foodFind/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/foodFind/')
