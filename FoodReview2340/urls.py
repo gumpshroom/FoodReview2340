6ec9dac342d8d46ba203b061cbf418faa2cac273
gdawerty <aaronluutri@gmail.com>
2024-09-16 01:03:45 -0400

    Connected the map button to the map app so it will display a map when clicked - September 15 Aaron Luu

--- FoodReview2340/urls.py
+++ FoodReview2340/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView  # Import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('foodFind.urls')),
+
 ]
