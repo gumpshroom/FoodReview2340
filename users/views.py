d9d9e191086a8172d44b1827b18e7900d560afe3
baonguyen1222 <85530882+baonguyen1222@users.noreply.github.com>
2024-09-17 15:15:03 -0400

    added email, first and last name to register

--- users/views.py
+++ users/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib import messages
+
+from users.forms import CustomUserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             login(request, form.save())
             return redirect('/foodFind/')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 def login_view(request):
