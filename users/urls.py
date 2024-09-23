076dc956650fa1aca24d1fe9dcb4120e7fe0b7d6
baonguyen1222 <85530882+baonguyen1222@users.noreply.github.com>
2024-09-22 22:43:47 -0400

    WIP favorites

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
-    path('favorites/', views.favorites_view, name='favorites'),
+    path('favorites/', views.favorites_list_view, name='favorites'),
+    path('addFavorites/', views.add_to_favorites_view, name='add-to-favorites'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Favorite
+import json
 
 from users.forms import CustomUserCreationForm
 
@@ -29,5 +33,38 @@ def logout_view(request):
     logout(request)
     return redirect('/foodFind/')
 
-def favorites_view(request):
-    return render(request, 'users/favorites.html')
+@login_required
+@csrf_exempt
+def add_to_favorites_view(request):
+    if request.method == 'POST':
+        data = request.json()
+        place_id = data.get('place_id')
+        name = data.get('name')
+        vicinity = data.get('vicinity')
+        rating = data.get('rating')
+    #    user_ratings_total = data.get('user_ratings_total')
+        cuisine = data.get('cuisine')
+
+        # Check if the restaurant is already in the user's favorites
+        if not Favorite.objects.filter(user=request.user, place_id=place_id).exists():
+            # Create and save the restaurant as a favorite
+            favorite = Favorite.objects.create(
+                user=request.user,
+                place_id=place_id,
+                name=name,
+                vicinity=vicinity,
+                rating=rating,
+    #            user_ratings_total=user_ratings_total,
+                cuisine=cuisine,
+            )
+            return JsonResponse({'success': True, 'message': 'Restaurant added to favorites!'})
+        else:
+            return JsonResponse({'success': False, 'message': 'Restaurant is already in your favorites!'})
+
+    return JsonResponse({'success': False, 'message': 'Invalid request'})
+
+@login_required
+def favorites_list_view(request):
+    favorites = Favorite.objects.filter(user=request.user)
+
+    return render(request, 'users/favorites.html', {'favorites': favorites})
