0e5b17262226ab02e8a42e9aa67743992ddac9d6
Eric Yang <eyang2023@gmail.com>
2024-09-24 19:47:01 -0400

    make search function work and load cards with info, add ai generated descriptions

--- FoodReview2340-main/foodFind/urls.py
+++ FoodReview2340-main/foodFind/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = "home"),
     path('map/', views.map, name='map'),
+    path('generateDescription/', views.generateDescription, name='generateDescription'),
 ]
